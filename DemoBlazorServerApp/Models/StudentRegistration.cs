766109e3edeb16a1ec3d755540ffd7d256ee3ecc
Dragos313 <pana749@gmail.com>
2024-01-14 23:23:24 +0200

    Finalizare adaugare studenti

--- DemoBlazorServerApp/Models/StudentRegistration.cs
+++ DemoBlazorServerApp/Models/StudentRegistration.cs
@@ -5,11 +5,11 @@
         public long StudentId { get; set; }
         public string FullName { get; set; }
 
-        public string UserId { get; set; }
+        public long UserId { get; set; }
         public string Email { get; set; }
         public string Specialization { get; set; }
 
-        public string SpecId { get; set; }
+        public long SpecId { get; set; }
         public string Tax { get; set; }
         public string Years { get; set; }
         public string RegisterDate { get; set; }
